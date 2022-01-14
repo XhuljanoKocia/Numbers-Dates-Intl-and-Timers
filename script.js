9ecbe45778c1d83386a07c418bc85757650bce7f
Lighto2stylo <53895514+Lighto2stylo@users.noreply.github.com>
2022-01-14 11:07:07 +0100

    Working with Dates

--- script.js
+++ script.js
@@ -382,3 +382,40 @@ btnSort.addEventListener('click', function (e) {
 
 // // Divisions
 // console.log(10n / 3n);
+
+/////////////////////////////////////////////////
+
+//CREATING DATES
+
+// Create a date
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('Jan 14 2022 10:50:47'));
+// console.log(new Date('December 24, 2015'));
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5)); // The months are 0 based
+
+// console.log(new Date(0));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay()); //The day of the week
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.getMilliseconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142253380000));
+
+console.log(Date.now()); //Gives a timestamp of the current moment
+
+console.log(future.setFullYear(2040));
+console.log(future);
