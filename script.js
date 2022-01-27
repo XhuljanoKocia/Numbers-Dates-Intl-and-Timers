9e4fdf730af3319b74c827f53bd59160d93602c3
Lighto2stylo <53895514+Lighto2stylo@users.noreply.github.com>
2022-01-27 17:38:20 +0100

    Dates & Times in Javascript

--- script.js
+++ script.js
@@ -594,3 +594,29 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 
 //IMPLEMENTING A COUNTDOWN TIMER
+
+/////////////////////////////////////////////////
+
+// //DATES & TIMES IN JAVASCRIPT
+// const now = new Date();
+// console.log(now);
+// console.log(typeof now);
+
+// // Year, Months, Day, Times
+// console.log('getFullYear:', now.getFullYear());
+// console.log('getMonth:', now.getMonth());
+// console.log('getDate:', now.getDate());
+// console.log('getDay:', now.getDay());
+// console.log('getHours:', now.getHours());
+// console.log('getMinutes:', now.getMinutes());
+// console.log('getSeconds:', now.getSeconds());
+
+// // Timestamps
+// console.log('Timestamp:', now.getTime());
+
+// // Date strings
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
+// console.log(now.toLocaleString());
+
+/////////////////////////////////////////////////
