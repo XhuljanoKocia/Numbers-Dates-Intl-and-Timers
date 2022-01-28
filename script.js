3520ba7e2a35f51471cc6b34901dc140a2b811ea
Lighto2stylo <53895514+Lighto2stylo@users.noreply.github.com>
2022-01-28 15:41:08 +0100

    Timestamps & Comparisons

--- script.js
+++ script.js
@@ -620,3 +620,27 @@ btnSort.addEventListener('click', function (e) {
 // console.log(now.toLocaleString());
 
 /////////////////////////////////////////////////
+
+// //TIMESTAMPS & COMPARISONS
+// const before = new Date('February 1 2019 7:30:59');
+// const now = new Date();
+
+// // console.log(now.getTime(), before.getTime());
+// const diff = now.getTime() - before.getTime();
+// console.log(diff);
+
+// const mins = Math.round(diff / 1000 / 60);
+// const hours = Math.round(mins / 60);
+// const days = Math.round(hours / 24);
+
+// console.log(mins);
+// console.log(hours);
+// console.log(days);
+
+// console.log(`The blog was written ${days} ago`);
+
+// // Converting timestamps into date objects
+// const timestamp = 1675938474990;
+// console.log(new Date(timestamp));
+
+/////////////////////////////////////////////////
