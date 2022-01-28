014b682b430e24f910fdbb54cc5275e199b7e2be
Lighto2stylo <53895514+Lighto2stylo@users.noreply.github.com>
2022-01-28 15:54:43 +0100

    Building a Digital Clock

--- script.js
+++ script.js
@@ -644,3 +644,25 @@ btnSort.addEventListener('click', function (e) {
 // console.log(new Date(timestamp));
 
 /////////////////////////////////////////////////
+
+// //BUILDING A DIGITAL CLOCK
+// const clock = document.querySelector('.clock');
+
+// const tick = () => {
+//   const now = new Date();
+//   const h = now.getHours();
+//   const m = now.getMinutes();
+//   const s = now.getSeconds();
+
+//   const html = `
+//     <span>${h}</span> :
+//     <span>${m}</span> :
+//     <span>${s}</span>
+//   `;
+
+//   clock.innerHTML = html;
+// };
+
+// setInterval(tick, 1000);
+
+/////////////////////////////////////////////////
